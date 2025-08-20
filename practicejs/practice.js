c9eb944f2279e3f70f0d0212f5777220692da3a9
Dinesh Pippera <dineshhh9550@gmail.com>
2025-08-20 21:57:35 +0530

    Add files via upload

---
+++ practicejs/practice.js
@@ -0,0 +1,11 @@
+let username;
+username=prompt("enter user name");
+let password;
+password=prompt("enter password")
+// if username && password{alert("both are correct")}
+if (username=="Dinesh" && password=="1234"){
+    alert("both are correct")
+}else{
+    alert("both are not coorct")
+}
+
