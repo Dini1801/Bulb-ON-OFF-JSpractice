f762ff0f75b3ece51f1b4b0c0d88452b9c522124
Dinesh Pippera <dineshhh9550@gmail.com>
2025-08-20 22:12:51 +0530

    practice.js

--- practicejs/practice.js
+++ practicejs/practice.js
@@ -1,11 +1,35 @@
-let username;
-username=prompt("enter user name");
-let password;
-password=prompt("enter password")
-// if username && password{alert("both are correct")}
-if (username=="Dinesh" && password=="1234"){
-    alert("both are correct")
-}else{
-    alert("both are not coorct")
-}
 
+let head=document.createElement("h1")
+document.body.appendChild(head)
+head.textContent=("Bulb ON/OFF")
+head.style.textAlign="center"
+
+let button = document.createElement("button")
+document.body.appendChild(button)
+button.textContent=("Turn On")
+button.style.margin="10px "
+button.style.backgroundColor="lightgreen"
+
+
+
+let button2 = document.createElement("button")
+document.body.appendChild(button2)
+button2.textContent=("Turn Off")
+button2.style.backgroundColor="red"
+
+
+
+let img=document.createElement("img")
+document.body.appendChild(img)
+img.src= "https://www.w3schools.com/js/pic_bulboff.gif"
+img.style.width="160px"
+img.style.margin="20px 10px 30px 80px"
+
+
+button.addEventListener("click",function(){
+    img.src= "https://www.w3schools.com/js/pic_bulbon.gif"
+})
+
+button2.addEventListener("click",function(){
+    img.src="https://www.w3schools.com/js/pic_bulboff.gif"
+})
